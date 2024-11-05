ee0e83e92f3f1ee10866a98486f23b646768b96e
Soviatul Choir Harahap <sofiaharahap5@gmail.com>
2024-11-05 14:02:12 +0700

    first commit

---
+++ routes/api.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\SupplierController;
+use App\Http\Controllers\OrderTransactionController;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+// Route::prefix('v1')->group(function () {
+
+// })
+
+
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::apiResource('categories', CategoryController::class);
+    Route::apiResource('suppliers', SupplierController::class);
+    Route::apiResource('products', ProductController::class);
+    Route::apiResource('transactions', OrderTransactionController::class);
+
+    Route::post('logout', [AuthController::class, 'logout']);
+});
+
+Route::get('user-list', [UserController::class, 'index']);
+
+
+Route::post('registration', [AuthController::class, 'registration']);
+Route::post('login', [AuthController::class, 'login']);
+
